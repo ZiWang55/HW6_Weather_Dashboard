6fee22064ec2fd42457c51e9def6345f2e4b2113
zi wang <ziwang55@gmail.com>
2020-11-03 11:35:21 -0600

    adding storage function into script.js

--- script.js
+++ script.js
@@ -8,6 +8,32 @@ initWeather();
 
 // This function displays the city entered by the user into the DOM
 function renderCities() {
-    $("#cityList").empty();
-    $("#cityInput").val("");
+  $("#cityList").empty();
+  $("#cityInput").val("");
+
+  for (i = 0; i < cityList.length; i++) {
+    var a = $("<a>");
+    a.addClass(
+      "list-group-item list-group-item-action list-group-item-primary city"
+    );
+    a.attr("data-name", cityList[i]);
+    a.text(cityList[i]);
+    $("#CityList").prepend(a);
+  }
+}
+
+// This function pulls the city list array from local storage
+function initCityList() {
+  var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+  if (storedCities !== null) {
+    cityList = storedCities;
+  }
+
+  renderCities();
+}
+
+// This function saves the currently display city to local storage
+function storeCurrentCity() {
+    localStorage.setItem("currentCity", JSON.stringify(cityname));
 }
